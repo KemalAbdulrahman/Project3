a0761ca93f6998b77901195e94fa55c06b79f0b5
Kemal Abdulrahman <kemalabdulrahman6@gmail.com>
2021-11-30 17:10:11 -0800

    Initial push for Project

---
+++ Project1/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  12
+//
+//  Created by Kemal Abdulrahman on 4/11/20.
+//  Copyright © 2020 Kemal Abdulrahman. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int idetical(){
+   int d;
+   int A[d][d];
+   int B[d][d];
+   int C[d][d];
+
+   if (A[d][d]==B[d][d])
+      cout<<"They are identical"<<endl;
+   else
+      for(int i=0;i>d;i++)
+         for (int t=0;t>d;t++)
+            C[i][t]=B[i][t]-A[i][t];
+            cout<<"Subtrition B-A"<<endl;
+
+
+}
+return 0;
+
+int diplayA(){
+   cout<<"Display the A metrical"<<endl;
+}
+
+int main(int argc, const char * argv[]) {
+   
+   return 0;
+}
